32dd0c07d209a29a167a20321da557b3ea7c6109
LeoraORISE-EPA <vegosen.leora@epa.gov>
2020-04-24 17:47:01 -0400

    Initial changes on ParseToxVal

--- src/gov/epa/ghs_data_gathering/Parse/ParseToxVal.java
+++ src/gov/epa/ghs_data_gathering/Parse/ParseToxVal.java
@@ -87,15 +87,116 @@ public class ParseToxVal extends Parse {
 					
 				Score score=null;
 				
-				if (r.risk_assessment_class.contentEquals("acute")) {
+				if (r.risk_assessment_class.contentEquals("acute") && r.human_eco.contentEquals("human health")) {
 					createAcuteMammalianToxicityRecords(chemical, r);
+					
+					/* Added: && r.human_eco.contentEquals("human health")
+					 * because there is at least one eco entry labeled "acute"
+					 * I seem to remember it should be && instead of & but need to check this.
+					 * -Leora 4/23/20 */
+					
+					
 				} else if (r.risk_assessment_class.contentEquals("cancer")) {
 					createCancerScore(chemical,r);
 				} else if (r.risk_assessment_class.contentEquals("developmental")) {
-					createDevelopmentalScore(chemical,r);				
+					createDevelopmentalScore(chemical,r);
+					
+					/* Added the rest of the rac here but need to add methods.  -Leora 4/24/20 */
+					
+				} else if (r.risk_assessment_class.contentEquals("mortality:acute")) {
+					createEcoToxAcuteScore(chemical,r);	
+				} else if (r.risk_assessment_class.contentEquals("mortality:chronic")) {
+					createEcoToxChronicScore(chemical,r);
+					
+					/* Need to double check that these are all eco.  -Leora 4/23/20 */
+					
+				} else if (r.risk_assessment_class.contentEquals("chronic")) {
+					createChronicScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("subchronic")) {
+					createSubchronicScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("short-term")) {
+					createShorttermScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("repeat dose")) {
+					createRepeatDoseScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("subacute")) {
+					createSubacuteScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("growth:acute")) {
+					createGrowthAcuteScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("growth:chronic")) {
+					createGrowthChronicScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("reproductive")) {
+					createReproductiveScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("neurotoxicity")) {
+					createNeurotoxicityScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("developmental neurotoxicity")) {
+					createDevelopmentalNeurotoxicityScore(chemical,r);
+					
+				} else if (r.risk_assessment_class.contentEquals("ecotoxicity invertebrate")) {
+					createEcotoxInvertebrateScore(chemical,r);
+					
+					
+					
 				} else {
 					//TODO add methods for other risk assessment classes
-					System.out.println("unknown rac="+r.risk_assessment_class);
+					/* System.out.println("unknown rac="+r.risk_assessment_class);
+					 * 
+					 * rac to add -Leora 4/23/20:
+					 * 
+					 * mortality:acute
+					 * mortality:chronic
+					 * 
+					 * chronic (human health: species include human and rat; eco: different aquatic species)
+					 * subchronic (human health: rat)
+					 * short-term
+					 * repeat dose
+					 * subacute
+					 * 
+					 * growth:acute
+					 * growth:chronic
+					 *
+					 * reproductive
+					 * 
+					 * neurotoxicity
+					 * developmental neurotoxicity
+					 * 
+					 * ecotoxicity invertebrate
+					 * 
+					 * 
+
+						Excellent! That’s what you are supposed to get because
+						code hasn’t been added to handle these risk assessment classes.
+						If you look at the code for the GoThroughRecords method in the
+						ParseToxVal class, you will see a block of code like this:
+						if (r.risk_assessment_class.contentEquals("acute")) {........
+						For right now you can comment out the line above that
+						prints unknown rac so that it doesn’t clutter the output.
+						So far I have only handled a few of the hazard categories
+						(and may not be complete yet).
+						
+						Look at the code for the first 3
+						I added and make sure I did it the same way as Richard.
+						I may have restricted the records more than he did-
+						for example for acute oral I only used “toxval_type”= “LD50”
+						and the 4 species that we used earlier in the ParseChemIdplus class.
+						
+						****[I think the point is to look at what Richard did and suggest
+						changes if we don't agree.  -Leora 4/23/20]****
+						
+						If you didn’t remember, pressing F3 will jump to a different method when
+						the cursor is on it. Alt + left arrow will go back to where you were.
+						Todd
+
+					 *  
+					 *  */
 					
 				}
 				
@@ -133,6 +234,17 @@ public class ParseToxVal extends Parse {
 		
 		} else {//need code for all important toxval_types
 			
+			/* NOAEL and cancer unit risk are the only toxval_types for acrylamide. -Leora 4/23/20
+			
+			/* Richard seemed to be suggesting that we treat all toxval_types the same and just use
+			If(min value < 1) high
+			Else if(min value < 10) medium
+			Else low
+			I'm just making a note of this for now, will come back to it.  -Leora 4/23/20
+ 
+*/
+			
+			
 		}
 
 		
@@ -143,6 +255,87 @@ public class ParseToxVal extends Parse {
 		
 	}
 
+	
+	private void createEcoToxAcuteScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+	}
+	
+	
+	private void createEcoToxChronicScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createChronicScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createSubchronicScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+
+	private void createShorttermScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+
+	private void createRepeatDoseScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createSubacuteScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createGrowthAcuteScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createGrowthChronicScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createReproductiveScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createNeurotoxicityScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createDevelopmentalNeurotoxicityScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	private void createEcotoxInvertebrateScore(Chemical chemical, RecordToxVal r) {
+		
+		// Need to add method.  -Leora 4/24/20
+		
+	}
+	
+	
+	
 	private void createAcuteMammalianToxicityInhalationRecord(Chemical chemical, RecordToxVal tr) {
 		// System.out.println("Creating AcuteMammalianToxicityInhalationRecord");
 
@@ -154,6 +347,13 @@ public class ParseToxVal extends Parse {
 		sr.route = "Inhalation";
 		
 		//TODO - do we only want to use LC50? I know richard might not restrict to LC50s
+		
+		/*
+		 * The Tiger Team didn't use acute tox by itself, only HER/BER/TER. Richard's
+		 * code doesn't specify LC50.
+		 * -Leora 4/24/20
+		 */
+		
 		if (!tr.toxval_type.contentEquals("LC50")) { 
 			System.out.println("invalid inhalation toxval_type="+tr.toxval_type);
 			return;
@@ -177,11 +377,35 @@ public class ParseToxVal extends Parse {
 		sr.valueMassOperator=tr.toxval_numeric_qualifier;
 		sr.valueMass = Double.parseDouble(tr.toxval_numeric);
 		sr.valueMassUnits = tr.toxval_units;
+		
+		/* Okay, I understand the code.  This basically renames what Richard called toxval_numeric
+		 * into valueMass and then valueMass is renamed score and then for acute toxicity,
+		 * so then for AcuteMammalianToxicity, the same code that we used for the
+		 * AA Dashboard/CHA Database is directly used.  -Leora 4/23/20  */
+		
 
 
 		if (!okSpecies.contains(tr.species_common))//TODO- does richard use all species???
 			return;
 
+		/* It looks like Richard's code doesn't mention species for acute tox.
+		 * So presumably all mammalian species in the ToxVal database were included for the Tiger Team.
+		 * 
+		 * But other work specified species. For example,
+		 * In Grace's Science Webinar presentation on 4/22/20, she talked about using ToxVal data to develop TTC.
+		 * She filtered from ToxVal:
+		 * toxval type: NO(A)EL or NO(A)EC
+		 * species: rats, mice, rabbits
+		 * To derive representative values, she removed outliers that exceeded the IQR.
+		 * 
+		 * For acrylamide, the only mammals with data for acute toxicity are the same mammals that we included
+		 * (rat, mouse, rabbit, guinea pig).
+		 * Need to check whether this is the case for all chemicals.
+		 * 
+		 * I think the more important thing for the code is to specify that the species_supercategory is mammals.
+		 * Need to add code to do this.
+		 * 
+		 * -Leora 4/23/20 */
 
 		setInhalationScore(sr, chemical);
 
@@ -231,7 +455,11 @@ public class ParseToxVal extends Parse {
 				sr.score = ScoreRecord.scoreL;
 				sr.rationale = "Inhalation LC50 (" + strDose + " mg/L) > 20 mg/L";
 			} else {
-				System.out.println(chemical.CAS + "\toral\t" + dose);
+				System.out.println(chemical.CAS + "\tinhalation\t" + dose);
+				/* Should "\toral\t" in the line above be "\tinhalation\t"?
+				 * Is this a mistake that is also in our ChemIDplus code?
+				 * -Leora 4/23/20 */
+				
 			}
 		} else {
 			System.out.println("Unknown operator: "+sr.valueMassOperator+" for acute inhalation");
@@ -485,13 +713,33 @@ public class ParseToxVal extends Parse {
 
 	}
 	
+	/* So this class is just for creating the individual scores.  Do we also want to integrate into one score
+	 * for each chemical?  Or is it actually best to not even do that so that we are not assigning a "final" score?
+	 * But integrating the scores is one of the main things that I've been contemplating for the ToxVal data.
+	 * As we discussed, it might make sense to use the priority_id field and take the minimum score from each of
+	 * the seven priority_id categories and then priority_id 1>2>3>4>5>6>7 in the trumping method.
+	 * Also, instead of, or in combination with, the trumping scheme, we could remove extreme outliers and
+	 * then take the minimum of the remaining scores.
+	 * Since the values are continuous instead of ordinal, removing outliers makes sense. 
+	 * 
+	 * In Grace's Science Webinar presentation on 4/22/20, she talked about using ToxVal data to develop TTC.
+	 * She filtered from ToxVal:
+	 * toxval type: NO(A)EL or NO(A)EC
+	 * species: rats, mice, rabbits
+	 * To derive representative values, she removed outliers that exceeded the IQR.
+	 *
+	 * -Leora 4/23/20  */
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
 		ParseToxVal p=new ParseToxVal();
 //		p.createFiles();
 		
-		String folder="E:\\Documents\\0000 epa\\0 telework\\AA dashboard";
+		String folder="C:\\Users\\Leora\\Desktop\\Tele\\ToxVal";
 		
 		String CAS="79-06-1";
 				
