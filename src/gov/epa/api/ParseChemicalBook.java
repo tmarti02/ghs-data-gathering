80b0acceecf1babed6d3bfdefe7a7ad911929091
DESKTOP-4BBCCV1\Weeb <ramsland.christian@epa.gov>
2020-11-20 09:33:54 -0500

    needed one of these

---
+++ src/gov/epa/api/ParseChemicalBook.java
@@ -0,0 +1,11 @@
+package gov.epa.api;
+
+import gov.epa.exp_data_gathering.parse.Parse;
+
+public class ParseChemicalBook extends Parse {
+	
+	public ParseChemicalBook() {
+		sourceName = "ChemicalBook";
+		this.init();
+	}
+}
