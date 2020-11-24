0341b1588ed6f8aefa5f46e750a8dd60e8e7bda5
AA\GSINCL01 <sinclair.gabriel@epa.gov>
2020-11-24 09:59:24 -0500

    no message

---
+++ src/gov/epa/exp_data_gathering/parse/ParseChemicalBook.java
@@ -0,0 +1,11 @@
+package gov.epa.exp_data_gathering.parse;
+
+public class ParseChemicalBook extends Parse {
+	
+	public ParseChemicalBook() {
+		sourceName = "ChemicalBook";
+		this.init();
+	}
+}
+
+// test
