5ad4b036778074d9c33908f02e3ba50c702b1b92
AA\GSINCL01 <sinclair.gabriel@epa.gov>
2020-12-02 10:51:39 -0500

    Improved efficiency and fixed errors in DriverOChem

--- src/gov/epa/exp_data_gathering/parse/DriverOChem.java
+++ src/gov/epa/exp_data_gathering/parse/DriverOChem.java
@@ -1,10 +1,15 @@
 package gov.epa.exp_data_gathering.parse;
 
+import java.util.List;
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import gov.epa.api.ExperimentalConstants;
 
@@ -14,7 +19,7 @@ public class DriverOChem {
 	 * Bulk selects and downloads records from OChem automatically
 	 * 
 	 * @param propertyName Use ExperimentalConstants
-	 * @param endIndex How many pages (100 records each) to download
+	 * @param endIndex How many pages (100 records each) to download; 0 to automatically download all
 	 * @param chromeDriverPath Path to your chromedriver.exe file
 	 */
 	public static void scrapeOChem(String propertyName,int endIndex,String chromeDriverPath) {
@@ -59,6 +64,7 @@ public class DriverOChem {
 			break;
 		}
 		
+		// Open new driver and connect to the OChem website
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://ochem.eu/");
@@ -67,90 +73,103 @@ public class DriverOChem {
 		try {
 			try {
 				// Click login button
-				WebElement login = driver.findElement(By.linkText("log in"));
+				WebElement login = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.linkText("log in")));
 				login.click();
-				Thread.sleep(5000);
 				
-				driver.switchTo().frame(2);
+				new WebDriverWait(driver,30000).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(2));
 				
 				// Log in as guest
-				WebElement button1 = driver.findElement(By.className("button-link"));
+				WebElement button1 = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.className("button-link")));
 				button1.click();
-				Thread.sleep(5000);
 				
 				// Accept terms
-				WebElement button2 = driver.findElement(By.className("button-link"));
+				WebElement button2 = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.className("button-link")));
 				button2.click();
-				Thread.sleep(5000);
 			} catch (Exception ex) {
 				System.out.println("Already logged in!");
 			}
 			
+			// Go to browser for selected property
 			String url = "https://ochem.eu/epbrowser/show.do?property="+propertyNumber;
 			driver.navigate().to(url);
-			Thread.sleep(15000);
 			
-			driver.switchTo().frame(2);
+			new WebDriverWait(driver,30000).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(2));
 			
 			// Display 100 records per page instead of default (5)
-			WebElement displayCount = driver.findElement(By.name("pagesize"));
+			WebElement displayCount = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.name("pagesize")));
 			displayCount.click();
 			displayCount.sendKeys("100" + Keys.ENTER);
-			Thread.sleep(15000);
 			
-			for (int i = 1; i <= endIndex; i++) {
-				// Select records on current page
-				WebElement selectAll = driver.findElement(By.cssSelector("[title=\"Select records on currently visible page\"]"));
+			// Wait for records to load
+			new WebDriverWait(driver,30000).until(ExpectedConditions.presenceOfElementLocated(By.className("browser-item")));
+			
+			// Scrape end index if not provided
+			if (endIndex==0) {
+				WebElement pager = driver.findElement(By.id("pager"));
+				List<WebElement> arrows = pager.findElements(By.tagName("a"));
+				int autoEndIndex = Integer.parseInt(arrows.get(1).getAttribute("page"));
+				endIndex = autoEndIndex;
+			}
+			
+			for (int i = 1; i <= endIndex; i++) {				
+				// Select all records
+				WebElement selectAll = new WebDriverWait(driver,30000)
+						.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[title=\"Select records on currently visible page\"]")));
 				selectAll.click();
-				Thread.sleep(20000);
+				
+				// Wait for records to reload after selecting
+				new WebDriverWait(driver,30000).until(ExpectedConditions.presenceOfElementLocated(By.className("browser-item")));
+				
+				// Double-check popup is gone before proceeding - order of loading seems to get messed up sometimes
+				new WebDriverWait(driver,30000).until(ExpectedConditions.attributeContains(By.id("waitingDialog_mask"),"style","display: none"));
 				
 				// Navigate to next page by typing page number into box (Charlie says this is most reliable)
-				if (i < endIndex) {
-					WebElement pageInput = driver.findElement(By.id("pageInput"));
+				if (i < endIndex) {	
+					WebElement pageInput = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.id("pageInput")));
 					pageInput.click();
-					Thread.sleep(2000);
-					
 					pageInput.clear();
-					Thread.sleep(2000);
-					
 					String nextPage = String.valueOf(i+1);
-					pageInput.clear();
 					pageInput.sendKeys(nextPage+Keys.ENTER);
-					Thread.sleep(15000);
+					
+					// Wait for records to load
+					new WebDriverWait(driver,30000).until(ExpectedConditions.presenceOfElementLocated(By.className("browser-item")));
 				}
 			}
 			
-			// View basket
+			// Go to basket static page
 			driver.navigate().to("https://ochem.eu/basket/show.do");
-			Thread.sleep(5000);
 			
-			driver.switchTo().frame(2);
+			new WebDriverWait(driver,30000).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(2));
 			
 			// Open record download menu and click export
-			WebElement recordMenu = driver.findElement(By.cssSelector("[src=\"img/icons/xls.gif\"]"));
+			WebElement recordMenu = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.cssSelector("[src=\"img/icons/xls.gif\"]")));
 			recordMenu.click();
-			WebElement exportBasket = driver.findElement(By.cssSelector("[action=\"basket\"]"));
+			WebElement exportBasket = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.cssSelector("[action=\"basket\"]")));
 			exportBasket.click();
-			Thread.sleep(2000);
 			
 			driver.switchTo().defaultContent();
-			driver.switchTo().frame(3);
+			new WebDriverWait(driver,30000).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(3));
 			
-			// Select options for export
-			WebElement externalID = driver.findElement(By.name("EXTERNAL_ID"));
+			// Select export options
+			WebElement externalID = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.name("EXTERNAL_ID")));
 			externalID.click();
-			WebElement articleN = driver.findElement(By.name("N"));
+			WebElement articleN = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.name("N")));
 			articleN.click();
-			WebElement pubID = driver.findElement(By.name("ARTICLE"));
+			WebElement pubID = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.name("ARTICLE")));
 			pubID.click();
-			WebElement selectUnits = driver.findElement(By.name("unit-"+propertyNumber));
+			WebElement comments = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.name("COMMENTS")));
+			comments.click();
+			WebElement selectUnits = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.name("unit-"+propertyNumber)));
 			selectUnits.click();
 			selectUnits.sendKeys(desiredUnits+Keys.ENTER);
 			
 			// Download!
-			WebElement getXLS = driver.findElement(By.cssSelector("[format=\"xls\"]"));
+			WebElement getXLS = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.cssSelector("[format=\"xls\"]")));
 			getXLS.click();
-			Thread.sleep(30000);
+			WebElement downloadButton = new WebDriverWait(driver,30000).until(ExpectedConditions.elementToBeClickable(By.className("fancy-button")));
+			String basketURL = downloadButton.getAttribute("href");
+			System.out.println(basketURL);
+			Thread.sleep(60000);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
@@ -159,7 +178,7 @@ public class DriverOChem {
 	}
 	
 	public static void main(String[] args) {
-		scrapeOChem(ExperimentalConstants.strMeltingPoint,2,"C:\\Users\\GSincl01\\Documents\\chromedriver.exe");
+		scrapeOChem(ExperimentalConstants.strHenrysLawConstant,0,"C:\\Users\\GSincl01\\Documents\\chromedriver.exe");
 	}
 	
 }
