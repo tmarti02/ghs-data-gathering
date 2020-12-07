f9bb4f837c169b8a6473dfaad49cc9da26a2ce00
AA\CRAMSLAN <ramsland.christian@epa.gov>
2020-12-07 14:05:58 -0500

    downloads Sander sites and fills a recordsander object with appropriate fields

--- src/gov/epa/exp_data_gathering/parse/ParseSander.java
+++ src/gov/epa/exp_data_gathering/parse/ParseSander.java
@@ -1,16 +1,28 @@
 package gov.epa.exp_data_gathering.parse;
 
+import java.io.File;
+import java.util.Vector;
+
 public class ParseSander extends Parse{
 	
 	public ParseSander() {
 		sourceName = "Sander";
 		this.init();
 	}
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Override
+	protected void createRecords() {
+		Vector<RecordSander> records = RecordSander.parseWebpagesInDatabase();
+		writeOriginalRecordsToFile(records);
 	}
+
+	
+	public static void main(String[] args) {
+		ParseSander p = new ParseSander();
+		p.mainFolder = p.mainFolder + File.separator + "General";
+		p.databaseFolder = p.mainFolder;
+		p.jsonFolder= p.mainFolder;
+		p.createFiles();
+	}
+	
 
 }
