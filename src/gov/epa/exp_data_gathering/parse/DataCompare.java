597c95d717d13be245c8ebec253e6ca69bd927a9
AA\GSINCL01 <sinclair.gabriel@epa.gov>
2021-01-06 15:52:56 -0500

    Tweaks to DataCompare to handle corrections for encoding issues

--- src/gov/epa/exp_data_gathering/parse/DataCompare.java
+++ src/gov/epa/exp_data_gathering/parse/DataCompare.java
@@ -2,17 +2,24 @@ package gov.epa.exp_data_gathering.parse;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Objects;
+
+import gov.epa.api.ExperimentalConstants;
 
 public class DataCompare {
 	public String sourceName1;
 	public String sourceName2;
 	private ExperimentalRecords records1;
 	private ExperimentalRecords records2;
+	private boolean includeBadRecords;
 	
 	public static final String mainFolder = "Data"+File.separator+"Experimental";
 
-	public DataCompare(String sourceName1,String sourceName2) {
+	public DataCompare(String sourceName1,String sourceName2,boolean includeBadRecords) {
+		this.includeBadRecords = includeBadRecords;
 		this.sourceName1 = sourceName1;
 		this.sourceName2 = sourceName2;
 		records1 = new ExperimentalRecords();
@@ -21,8 +28,10 @@ public class DataCompare {
 		try {
 			ExperimentalRecords records = ExperimentalRecords.loadFromJSON(record1FileName);
 			if (records!=null) { records1.addAll(records); }
-			ExperimentalRecords badRecords = ExperimentalRecords.loadFromJSON(badRecord1FileName);
-			if (badRecords!=null) { records1.addAll(badRecords); }
+			if (includeBadRecords) {
+				ExperimentalRecords badRecords = ExperimentalRecords.loadFromJSON(badRecord1FileName);
+				if (badRecords!=null) { records1.addAll(badRecords); }
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -33,14 +42,16 @@ public class DataCompare {
 		try {
 			ExperimentalRecords records = ExperimentalRecords.loadFromJSON(record2FileName);
 			if (records!=null) { records2.addAll(records); }
-			ExperimentalRecords badRecords = ExperimentalRecords.loadFromJSON(badRecord2FileName);
-			if (badRecords!=null) { records2.addAll(badRecords); }
+			if (includeBadRecords) {
+				ExperimentalRecords badRecords = ExperimentalRecords.loadFromJSON(badRecord2FileName);
+				if (badRecords!=null) { records2.addAll(badRecords); }
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 	
-	public ExperimentalRecords compare() {
+	private ExperimentalRecords compare() {
 		ExperimentalRecords comp = new ExperimentalRecords();
 		List<ExperimentalRecord> recordsIn1NotIn2 = new ArrayList<ExperimentalRecord>(records1);
 		List<ExperimentalRecord> recordsIn2ToRemove = new ArrayList<ExperimentalRecord>(records2);
@@ -50,19 +61,74 @@ public class DataCompare {
 		List<ExperimentalRecord> recordsIn1ToRemove = new ArrayList<ExperimentalRecord>(records1);
 		recordsIn2NotIn1.removeAll(recordsIn1ToRemove);
 		System.out.println("Found "+recordsIn2NotIn1.size()+" records in "+sourceName2+" missing from "+sourceName1+".");
+		if (sourceName1.equals(ExperimentalConstants.strSourceEChemPortal) && sourceName2.equals(ExperimentalConstants.strSourceEChemPortalAPI)) {
+			eChemPortalCorrection(recordsIn1NotIn2,recordsIn2NotIn1);
+		}
 		comp.addAll(recordsIn1NotIn2);
 		comp.addAll(recordsIn2NotIn1);
 		return comp;
 	}
 	
+	private void eChemPortalCorrection(List<ExperimentalRecord> inExcelNotInAPI,List<ExperimentalRecord> inAPINotInExcel) {
+		Iterator<ExperimentalRecord> iti = inExcelNotInAPI.iterator();
+		while (iti.hasNext()) {
+			ExperimentalRecord reci = iti.next();
+			if (reci.property_name.equals(ExperimentalConstants.strDensity)) {
+				// Original eChemPortal density download was missing criteria, so cannot be matched
+				iti.remove();
+				continue;
+			}
+			boolean remove = false;
+			Iterator<ExperimentalRecord> itj = inAPINotInExcel.iterator();
+			while (itj.hasNext()) {
+				ExperimentalRecord recj = itj.next();
+				if (identifiersMatch(reci,recj) && reci.property_name.equals(recj.property_name)) {
+					// Accounts for false mismatches due to character encoding problems
+					int valueIndex = reci.property_value_string.contains(";") ? reci.property_value_string.indexOf(";") : reci.property_value_string.length();
+					String fixedPropertyValueString = reci.property_value_string.substring(0,valueIndex).replaceAll("\\?","-");
+					if (recj.property_value_string.contains(fixedPropertyValueString) && conditionsMatch(reci,recj)) {
+						remove = true;
+						itj.remove();
+						break;
+					}
+				}
+			}
+			if (remove) { iti.remove(); }
+		}
+	}
+	
+	private static boolean identifiersMatch(ExperimentalRecord reci, ExperimentalRecord recj) {
+		if (Objects.equals(reci.casrn, recj.casrn) || Objects.equals(reci.einecs, recj.einecs) || Objects.equals(reci.chemical_name,  recj.chemical_name)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private static boolean conditionsMatch(ExperimentalRecord reci, ExperimentalRecord recj) {
+		if ((Objects.equals(reci.pressure_mmHg, recj.pressure_mmHg) || reci.pressure_mmHg==null) &&
+				(Objects.equals(reci.temperature_C, recj.temperature_C) || reci.temperature_C==null) &&
+				(Objects.equals(reci.pH, recj.pH) || reci.pH==null)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	public void compareAndWriteFiles() {
 		ExperimentalRecords comp = compare();
-		comp.toJSON_File(mainFolder+File.separator+sourceName1+"_"+sourceName2+"_RecordCompare.json");
-		comp.toExcel_File(mainFolder+File.separator+sourceName1+"_"+sourceName2+"_RecordCompare.xlsx");
+		String noteBadRecords = includeBadRecords ? "_WithBadRecords" : "_NoBadRecords";
+		String jsonFilepath = mainFolder+File.separator+sourceName1+"_"+sourceName2+"_Comparison"+noteBadRecords+".json";
+		String excelFilepath = mainFolder+File.separator+sourceName1+"_"+sourceName2+"_Comparison"+noteBadRecords+".xlsx";
+		System.out.println("Writing to JSON file...");
+		comp.toJSON_File(jsonFilepath);
+		System.out.println("Writing to Excel file...");
+		comp.toExcel_File(excelFilepath);
+		System.out.println("Done!");
 	}
 	
 	public static void main(String[] args) {
-		DataCompare d = new DataCompare("eChemPortal","eChemPortalAPI");
+		DataCompare d = new DataCompare("eChemPortal","eChemPortalAPI",true);
 		d.compareAndWriteFiles();
 	}
 }
