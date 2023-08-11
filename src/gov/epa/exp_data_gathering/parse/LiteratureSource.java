2a0fb61142004dc68c2d7b92a3f35c3d08bcf55d
AA\TMARTI02 <martin.todd@epa.gov>
2023-08-11 16:59:05 -0400

    Adding literature source

---
+++ src/gov/epa/exp_data_gathering/parse/LiteratureSource.java
@@ -0,0 +1,20 @@
+package gov.epa.exp_data_gathering.parse;
+
+
+public class LiteratureSource {
+	
+	public String name;
+	
+	//to be consistent with ToxVal
+	public String citation;	
+	public String title;
+	public String author;
+	public String journal;
+	public String volume;
+	public String year;
+	public String url;
+	public String doi;
+	public String notes;
+	
+//	public String documentName;
+}
